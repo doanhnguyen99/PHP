b0a501a25ea183b156aa69a4babaa1a3e3411385
Doanh <52314607+doanhnguyen99@users.noreply.github.com>
2019-11-06 00:48:59 +0700

    Fixed validated

--- Laravel-Crud/resources/views/student/create.blade.php
+++ Laravel-Crud/resources/views/student/create.blade.php
@@ -8,12 +8,13 @@
         <br>
 
         @if(count($errors) > 0)
-        <div class="alert alert-danger"></div>
+        <div class="alert alert-danger">
             <ul>
-                @foreach($errors->all() as $errors)
+                @foreach($errors->all() as $error)
                     <li>{{$error}}</li>
                 @endforeach
             </ul>
+        </div>
         @endif
 
         @if(\Session::has('Success'))

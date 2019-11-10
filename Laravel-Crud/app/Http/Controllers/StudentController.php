5c9d8a84a026dcf844edf9a822b0e05527b869b2
Doanh <52314607+doanhnguyen99@users.noreply.github.com>
2019-11-10 12:44:13 +0700

    Update or edit data into db

--- Laravel-Crud/app/Http/Controllers/StudentController.php
+++ Laravel-Crud/app/Http/Controllers/StudentController.php
@@ -72,7 +72,8 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        //
+       $student = Student::find($id);
+       return view('student.edit', compact('student', 'id'));
     }
 
     /**
@@ -84,7 +85,17 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'first_name' => 'required',
+            'last_name' => 'required'
+        ]);
+
+        $student = Student::find($id);
+        $student->first_name = $request->get('first_name');
+        $student->last_name = $request->get('last_name');
+        $student->save();
+
+        return redirect()->route('student.index')->with('Success', 'Data Updated');
     }
 
     /**
@@ -95,6 +106,8 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $student = Student::find($id);
+        $student->delete();
+        return redirect()->route('student.index')->with('Success', 'Delete Data');
     }
 }
--- Laravel-Crud/resources/views/student/index.blade.php
+++ Laravel-Crud/resources/views/student/index.blade.php
@@ -8,6 +8,12 @@
         <h3 text-align="center">Student Data</h3>
         <br>
 
+        <!-- @if($message = Session::get('Success'))
+        <div class="alert alert-success">
+            <p>{{@message}}</p>
+        </div>
+        @endif -->
+
         <div align="right">
         <a href="{{route('student.create')}}" class="btn btn-primary">Add</a>
         </div>
@@ -23,12 +29,33 @@
                 <tr>
                     <td>{{$row['first_name']}}</td>
                     <td>{{$row['last_name']}}</td>
-                    <td></td>
-                    <td></td>
+                    <td>
+                        <a href="{{action('StudentController@edit', $row['id'])}}" class="btn btn-warning">Edit</a>
+                    </td>
+                    <td>
+                        <form method="post" class="delete_form" action="action{{('StudentController@destroy, $row['id'])}}">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="DELETE">
+                            <button type="submit" class="btn btn-danger">Delete</button>
+
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </table>
     </div>
 </div>
+
+<script>
+    $(document).ready(function(){
+        $('.delete_form').on('submit', function(){
+            if('You are sure you want to delete it?'){
+                return true;
+            }else{
+                return false;
+            }
+        });
+    });
+</script>
 
 @endsection

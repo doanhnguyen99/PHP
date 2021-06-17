3643115930cd970c1caf4defa3900279efd55c1b
doanhnguyen99 <doanh080199@gmail.com>
2021-06-17 23:54:13 +0700

    seed data

---
+++ Laravel/app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model implements Authenticatable
+{
+    use HasFactory;
+    use AuthenticableTrait;
+}
